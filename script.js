5c7da6341f3cca0abd4c32b8239a4c296d327fd0
Parmeshwar yadav <yadavparmeshwar342@gmail.com>
2023-02-19 21:35:33 +0530

    updated - js

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
+const weatherStatus = document.getElementById("weatherStatus");
+
+const cityName = document.getElementById("cityName");
+const cityNameWT = cityName.value;
+
 // sets the background image from unsplash API.
 const backgroundImage = async () => {
   // Fetch Details
   const fetchData = await fetch(
-    `https://api.unsplash.com/search/photos?page=1&query=rain&client_id=WpcvfYB4CrJFW3URGI7zUGbHukXU-kkbKgssMHCaVHU`
+    `https://api.unsplash.com/search/photos?page=1&query=${cityNameWT}&client_id=WpcvfYB4CrJFW3URGI7zUGbHukXU-kkbKgssMHCaVHU`
   );
 
   const APIData = await fetchData.json();
 
   // Displaying the data in HTML
   const background = document.querySelector("body");
-  background.style.backgroundImage = `url(${APIData.results[0].urls.full})`;
+  background.style.backgroundImage = `url(${APIData.results[4].urls.full})`;
 };
 backgroundImage();
 
-let data;
-
+//targeting the element.
 const inputBox = document.getElementById("inputBox");
 const countryName = document.getElementById("countryName");
 const stateName = document.getElementById("stateName");
-
-// Data => Done
-// Variable to Store the Element => Done
-// Function to get the data from weather app
-// Manipluate the varibe of already created element
-const cityName = document.getElementById("cityName");
 const humidity = document.getElementById("humidity");
 const windSpeed = document.getElementById("windSpeed");
 const temprature = document.getElementById("temprature");
 const logoImage = document.getElementById("logoImage");
-const weatherStatus = document.getElementById("weatherStatus");
 
+// Function to get the data from weather app
 // preventing default behaviour
 const getData = async (event) => {
   event.preventDefault();
@@ -47,7 +45,7 @@ const getData = async (event) => {
   );
 
   const orgData = await fetchData.json();
-  data = orgData;
+  let data = orgData;
 
   // Displaying the data in HTML
   countryName.innerHTML = data.location.country;
